db41083e19585198f7f882af6e33a43d8adf3a9a
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:57:35 -0300

    Delete Desafio027.py

--- Desafio027.py
+++
@@ -1,7 +0,0 @@
-"""Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome
-separadamente."""
-nome = str(input('Nome: ')).strip().split()
-primeiro = nome[0]
-print(primeiro)
-ultimo = nome[-1]
-print(ultimo)
