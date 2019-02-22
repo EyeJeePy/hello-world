2637f8f97a8552256a6c5824696b41437bae6557
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-22 19:07:33 -0300

    Update Desafio060_Fatorial.py

--- Mundo 2/Desafio060_Fatorial.py
+++ Mundo 2/Desafio060_Fatorial.py
@@ -1,3 +1,10 @@
 """Faça um programa que leia um número qualquer e mostre o seu fatorial.
 Exemplo: 5! = 5 x 4 x 3 x 2 x 1 = 120"""
-
+fac = 1
+num = int(input('Insira um número para obter seu fatorial: '))
+while num > 0:
+    print('{}'.format(num), end='')
+    print(' x ' if num > 1 else ' = ', end='')
+    fac *= num
+    num -= 1
+print('{}'.format(fac))
