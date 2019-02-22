ea8fb395d5e0297b4b4154e5dc3175d0bda5ab27
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:55:38 -0300

    Delete Desafio018.py

--- Desafio018.py
+++
@@ -1,6 +0,0 @@
-"""Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo."""
-from math import sin, cos, tan
-a = float(input('Insira um ângulo: '))
-print("Sen = {}".format(sin(a)))
-print("Cos = {}".format(cos(a)))
-print("Tan = {}".format(tan(a)))
