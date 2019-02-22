6e7a71ad6cc84c3266afbe775b03fde06ecb8bcd
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:55:18 -0300

    Delete Desafio016.py

--- Desafio016.py
+++
@@ -1,5 +0,0 @@
-"""Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira."""
-import math
-n = float(input('Digite um número real qualquer: '))
-ni = math.trunc(n)
-print('Sua porção inteira é {}.'.format(ni))
