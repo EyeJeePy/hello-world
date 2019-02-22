0ff0831791346b9c65b891d1f9dac9488cd39c64
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:55:25 -0300

    Delete Desafio017.py

--- Desafio017.py
+++
@@ -1,7 +0,0 @@
-"""Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo,
-calcule e mostre o comprimento da hipotenusa."""
-from math import sqrt
-co = float(input('Cateto Oposto = '))
-ca = float(input('Cateto Adjacente = '))
-h = sqrt(pow(co, 2) + pow(ca, 2))
-print('A hipotenusa mede {}.'.format(h))
