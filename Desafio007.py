64b23791c2dd4ed707a8e3bbb46a21609d330a23
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:53:19 -0300

    Delete Desafio007.py

--- Desafio007.py
+++
@@ -1,9 +0,0 @@
-# Desenvolva um programa que leia as notas de um aluno, calcule e mostre a sua média. #
-p = float(input('Português: '))
-m = float(input('Matemática: '))
-h = float(input('História: '))
-g = float(input('Geografia: '))
-c = float(input('Ciências: '))
-ma = (p+m+h+g+c)/5
-mi = (p+m+h+g+c)//5
-print('A média é {}, a média inteira é {}'.format(ma, mi))
