949bad08093eeec7284e0b94e091f9e34888bbfd
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:54:53 -0300

    Delete Desafio011.py

--- Desafio011.py
+++
@@ -1,6 +0,0 @@
-"""Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinta
-necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m²."""
-lar = float(input('Largura da parede em metros: '))
-hei = float(input('Altura da parede em metros: '))
-print('A parede tem {}m².'.format(lar*hei), end=' ')
-print('Serão necessários {}L de tinta para pintá-la.'.format((lar*hei)/2))
