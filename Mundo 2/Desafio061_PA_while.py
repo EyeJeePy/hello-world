1120f8f342e988d82d9d47a6597fafbd111dd922
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-22 19:42:16 -0300

    Update Desafio061_PA_while.py

--- Mundo 2/Desafio061_PA_while.py
+++ Mundo 2/Desafio061_PA_while.py
@@ -1,2 +1,13 @@
 """Refaça o Desafio051 lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
 usando a estrutura while."""
+print('\033[7m === CALCULADORA DE PROGRESSÃO ARITMÉTICA === \033[0m')
+c = 0
+num = int(input('Insira o primeiro termo: '))
+raz = int(input('Insira a razão: '))
+ter = int(input('Insira quantos termos: '))
+print('{}'.format(num), end=' → ')
+while c < ter:
+    c += 1
+    num += raz
+    print('{}'.format(num), ' → ' if c < ter else '', end='')
+print('\n\n\033[7m =================== FIM =================== \033[0m')
