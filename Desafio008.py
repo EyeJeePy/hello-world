044a5777806d63f947fd083e52bdf1529221b352
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:53:26 -0300

    Delete Desafio008.py

--- Desafio008.py
+++
@@ -1,5 +0,0 @@
-"""Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros."""
-mt = float(input('Insira um valor em metros: '))
-c = mt * 10
-mm = mt * 100
-print('Ele equivale a {}cm ou {}mm.'.format(c, mm))
