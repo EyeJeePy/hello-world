7e6eeebc9bb41cad16dbfa486e784c4434fe5583
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:57:57 -0300

    Delete Desafio030.py

--- Desafio030.py
+++
@@ -1,9 +0,0 @@
-"""Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar."""
-num = int(input('Insira um número inteiro: '))
-div = (num % 2)
-
-if div == 0:
-    print('O número {} é par.'.format(num))
-
-else:
-    print('O número {} é ímpar.'.format(num))
