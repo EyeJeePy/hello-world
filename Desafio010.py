166c51d79ab5165dfd2fa6a0e2fa1c179007c42d
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:54:46 -0300

    Delete Desafio010.py

--- Desafio010.py
+++
@@ -1,4 +0,0 @@
-"""Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
-Considere US$1,00 = R$3,27."""
-r = float(input('Quantos reais você tem na carteira? R$'))
-print('Com R${:.3f}, você conseguiria comprar US${:.3f}.'.format(r, r/3.27))
