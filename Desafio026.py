3970469d1aad8a4edf3c4de460831a5190747637
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-21 22:57:27 -0300

    Delete Desafio026.py

--- Desafio026.py
+++
@@ -1,9 +0,0 @@
-"""Faça um programa que leia uma frase pelo teclado e mostre:
-- Quantas vezes aparece a letra "A";
-- Em que posição ela aparece a primeira vez;
-- Em que posição ela aparece a última vez."""
-frase = str(input('Frase: ')).strip().lower()
-print('\nTotal de {} "A".'.format(frase.count('a')))
-print('O "A" aparece pela primeira vez na posição {}.'.format(frase.find('a')))
-a = frase.rfind('a')
-print('O último "A" aparece na posição {}.'.format(a))
