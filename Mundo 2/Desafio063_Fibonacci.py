79ed63aa8985f53ee8a50da9c063e1feec7b57e0
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-23 16:34:10 -0300

    Update Desafio063_Fibonacci.py

--- Mundo 2/Desafio063_Fibonacci.py
+++ Mundo 2/Desafio063_Fibonacci.py
@@ -1,3 +1,17 @@
 """Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma Sequência
 de Fibonacci.
 Exemplo: 0 → 1 → 1 → 2 → 3 → 5 → 8"""
+# F(x) = F(x-1) + F(x-2)
+c = 0
+t1 = 0
+t2 = 1
+n = int(input('Número de elementos: '))
+print('{} >>> {}'.format(t1, t2), end='')
+
+while c <= n:
+    t3 = t1 + t2
+    print(' >>> {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    c += 1
+print('\nFIM')
