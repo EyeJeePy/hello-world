96d98293870898aba6e6932b052de4b2a2066822
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-23 16:58:06 -0300

    Update Desafio064_999.py

--- Mundo 2/Desafio064_999.py
+++ Mundo 2/Desafio064_999.py
@@ -1,3 +1,14 @@
 """Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o
 valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
 (desconsiderando o flag)."""
+c = 0
+n = 0
+s = 0
+n = float(input('Somador de Entradas\n'
+                'Insira o número [999 para sair]: '))
+while n != 999:
+    s += n
+    c += 1
+    print('>>> {}'.format(s))
+    n = float(input('Insira o número [999 para sair]: '))
+print('Você digitou {} números cuja soma é {}.'.format(c, s))
