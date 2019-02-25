69ac89d163b2415fb178f738f017dc55434744fb
Isa <47872520+EyeJeePy@users.noreply.github.com>
2019-02-25 03:40:14 -0300

    Update Desafio065_Maior_menor.py

--- Mundo 2/Desafio065_Maior_menor.py
+++ Mundo 2/Desafio065_Maior_menor.py
@@ -1,3 +1,24 @@
 """Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os
 valores e qual foi o maior e o menor valor lido. O programa deve perguntar ao usuário se ele quer ou não continuar a
 digitar valores."""
+maior = 0
+menor = 0
+count = 0
+opt = 'Ss'
+med = 0
+soma = 0
+while opt in 'Ss':
+    num = int(input('Insira um número: '))
+    count += 1
+    soma += num
+    if count == 1:
+        maior = num
+        menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    opt = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+med = soma / count
+print('{}, {}, {}'.format(med, maior, menor))
